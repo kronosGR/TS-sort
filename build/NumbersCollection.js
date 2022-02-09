00e02d3296cd3f0e252881a3fd60f7a349a876fa
KronosGR <geo.elgeo@gmail.com>
2022-02-09 20:07:25 +0100

    add NumbersCollection and refactored Sorter Class

---
+++ build/NumbersCollection.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+class NumbersCollection {
+    constructor(data) {
+        this.data = data;
+    }
+    // getter and you call it as property and not method
+    get length() {
+        return this.data.length;
+    }
+    compare(leftIndex, rightIndex) {
+        return this.data[leftIndex] > this.data[rightIndex];
+    }
+    swap(leftIndex, rightIndex) {
+        const leftHand = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex];
+        this.data[rightIndex] = leftHand;
+    }
+}
+exports.NumbersCollection = NumbersCollection;
---
+++ src/NumbersCollection.ts
@@ -0,0 +1,19 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  // getter and you call it as property and not method
+  get length():number{
+    return this.data.length
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+
+}
