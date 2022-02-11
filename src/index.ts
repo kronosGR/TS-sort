471fbb5ee87c3b5910537fedab4d35ca2f968af3
KronosGR <geo.elgeo@gmail.com>
2022-02-11 09:17:22 +0100

    inherited Sorter to all collections

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([150, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const characterCollection = new CharactersCollection('Xaayb');
-const sorterC = new Sorter(characterCollection);
-sorterC.sort();
+characterCollection.sort();
 console.log(characterCollection.data)
 
 const linkedList = new LinkedList();
@@ -19,7 +17,6 @@ linkedList.add(-500);
 linkedList.add(50);
 linkedList.add(-2);
 
-const sorterL = new Sorter(linkedList);
-sorterL.sort();
+linkedList.sort();
 linkedList.print()
 
